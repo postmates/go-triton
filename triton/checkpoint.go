640050a3fc4b36c2996d2b0770ae6c546c2908da
Rhett Garber <rhettg@gmail.com>
2015-07-08 17:17:48 -0700

    Use SQL for checkpointing

--- triton/checkpoint.go
+++ triton/checkpoint.go
@@ -1,40 +1,112 @@
 package triton
 
 import (
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"database/sql"
+	"fmt"
+
+	"github.com/mattn/go-sqlite3"
 )
 
 type Checkpointer struct {
 	clientName string
-	streamName string
-	shardID    string
+	stream     *Stream
 
-	db DynamoDBService
+	db *sql.DB
 }
 
+const CREATE_TABLE = `
+CREATE TABLE triton_checkpoint (
+	client VARCHAR(255),
+	stream VARCHAR(255),
+	shard VARCHAR(255),
+	seq_num VARCHAR(255),
+	PRIMARY KEY (client, stream, shard))
+`
+
 func (c *Checkpointer) Checkpoint() (err error) {
-	// iu := make(map[string]*dynamodb.AttributeValueUpdate)
-
-	key := make(map[string]*dynamodb.AttributeValue)
-	key["clientName"] = &dynamodb.AttributeValue{S: aws.String(c.clientName)}
-
-	pi := dynamodb.UpdateItemInput{
-		TableName: aws.String("triton"),
-		Key:       key,
-		//Item: item,
+	txn, err := c.db.Begin()
+	if err != nil {
+		return err
 	}
 
-	_, err = c.db.UpdateItem(&pi)
+	rows, err := txn.Query(
+		"SELECT 1 FROM triton_checkpoint WHERE client=$1 AND stream=$2 AND shard=$3",
+		c.clientName, c.stream.StreamName, c.stream.ShardID)
+	if err != nil {
+		txn.Rollback()
+		return err
+	}
+
+	defer rows.Close()
+
+	if rows.Next() {
+		fmt.Println("Do UPDATE")
+		res, err := txn.Exec(
+			"UPDATE triton_checkpoint VALUES seq_num=$4 WHERE client=$1 AND stream=$2 AND shard=$3",
+			c.clientName, c.stream.StreamName, c.stream.ShardID, c.stream.LastSequenceNumber)
+		if err != nil {
+			txn.Rollback()
+			return err
+		}
+
+		n, err := res.RowsAffected()
+		if n <= 0 {
+			txn.Rollback()
+			panic("Should have updated rows")
+		}
+
+	} else {
+		fmt.Println("Do INSERT")
+		_, err := txn.Exec(
+			"INSERT INTO triton_checkpoint VALUES ($1, $2, $3, $4)",
+			c.clientName, c.stream.StreamName, c.stream.ShardID, c.stream.LastSequenceNumber)
+
+		if err != nil {
+			txn.Rollback()
+			panic(err)
+		}
+	}
+
+	err = txn.Commit()
 
 	return
 }
 
-func NewCheckpointer(clientName string, stream *Stream, db DynamoDBService) *Checkpointer {
+func checkDB(db *sql.DB) bool {
+	_, err := db.Exec("select 1 from triton_checkpoint")
+	// TODO: pgsql
+
+	if sErr, ok := err.(sqlite3.Error); ok { // Now the error number is accessible directly
+		if sErr.Code == sqlite3.ErrError {
+			return false
+		}
+
+		panic(err)
+	}
+
+	return true
+}
+
+func initDB(db *sql.DB) (err error) {
+	_, err = db.Exec(CREATE_TABLE)
+	return
+}
+
+func NewCheckpointer(clientName string, stream *Stream, db *sql.DB) *Checkpointer {
+	if !checkDB(db) {
+		err := initDB(db)
+		if err != nil {
+			panic(err)
+		}
+
+		if !checkDB(db) {
+			panic(fmt.Errorf("Failed with db"))
+		}
+	}
+
 	c := Checkpointer{
 		clientName: clientName,
-		streamName: stream.StreamName,
-		shardID:    stream.ShardID,
+		stream:     stream,
 		db:         db,
 	}
 
--- triton/checkpoint_test.go
+++ triton/checkpoint_test.go
@@ -1,35 +1,40 @@
 package triton
 
 import (
+	"database/sql"
+	"os"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-type NullDynamoDBService struct {
-	// Just enough to record the last Update
-	Key              map[string]*dynamodb.AttributeValue
-	AttributeUpdates map[string]*dynamodb.AttributeValueUpdate
+const DB_URL string = "test.db"
+
+func openTestDB() *sql.DB {
+	db, err := sql.Open("sqlite3", DB_URL)
+	if err != nil {
+		panic(err)
+	}
+
+	return db
 }
 
-func (s *NullDynamoDBService) UpdateItem(input *dynamodb.UpdateItemInput) (*dynamodb.UpdateItemOutput, error) {
-	s.Key = input.Key
-	s.AttributeUpdates = input.AttributeUpdates
-
-	o := dynamodb.UpdateItemOutput{}
-
-	return &o, nil
+func closeTestDB(db *sql.DB) {
+	db.Close()
+	os.Remove(DB_URL)
 }
 
 func TestNewCheckpointer(t *testing.T) {
+	db := openTestDB()
+	defer closeTestDB(db)
+
 	streamName := "test-stream"
 	shardID := "shard-0000"
 	ksvc := NullKinesisService{}
 	s := NewStream(&ksvc, streamName, shardID)
 
-	db := NullDynamoDBService{}
-	c := NewCheckpointer("test", s, &db)
+	c := NewCheckpointer("test", s, db)
 
 	if c == nil {
 		t.Errorf("Failed to create")
@@ -39,33 +44,23 @@ func TestNewCheckpointer(t *testing.T) {
 	if c.clientName != "test" {
 		t.Errorf("Name mismatch")
 	}
-
-	if c.streamName != streamName {
-		t.Errorf("StreamName mismatch")
-	}
-
-	if c.shardID != shardID {
-		t.Errorf("shardID mismatch")
-	}
 }
 
 func TestCheckpoint(t *testing.T) {
+	db := openTestDB()
+	defer closeTestDB(db)
+
 	streamName := "test-stream"
 	shardID := "shard-0000"
 	ksvc := NullKinesisService{}
 	s := NewStream(&ksvc, streamName, shardID)
 
-	db := NullDynamoDBService{}
-	c := NewCheckpointer("test", s, &db)
+	c := NewCheckpointer("test", s, db)
 
 	s.LastSequenceNumber = aws.String("1234")
 
-	c.Checkpoint()
-
-	if db.Key == nil {
-		t.Errorf("No update")
-		return
+	err := c.Checkpoint()
+	if err != nil {
+		t.Errorf("Failed to checkpoint: %v", err)
 	}
-
-	t.Errorf("Found %v", db.Key)
 }
